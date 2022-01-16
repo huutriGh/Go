f6407aa7a7954ab0b3af1ba6f349035c71f6b700
Nguyễn Hữu Trí <huutriqt13@hotmail.com>
2022-01-16 20:28:28 +0700

    operator continue

--- operators/main.go
+++ operators/main.go
@@ -2,35 +2,69 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	//Multiplication
 	// area =mr2
 
-	var radius = 12.0
-	area := math.Pi * radius * radius
-	fmt.Println("area is", area)
+	// var radius = 12.0
+	// area := math.Pi * radius * radius
+	// fmt.Println("area is", area)
 
-	// iteger division
+	// // iteger division
 
-	half := 1 / 2
-	fmt.Println("half with integer division:", half)
-	halfFloat := 1.0 / 2.0
-	fmt.Println("half float:", halfFloat)
+	// half := 1 / 2
+	// fmt.Println("half with integer division:", half)
+	// halfFloat := 1.0 / 2.0
+	// fmt.Println("half float:", halfFloat)
 
-	// squaring (raising to the power)
+	// // squaring (raising to the power)
 
-	badThreeSquared := 3 ^ 2
-	fmt.Println("badThreeSquared:", badThreeSquared)
-	goodThreeSquared := int(math.Pow(3.0, 2.0))
-	fmt.Println("goodThreeSquare:", goodThreeSquared)
+	// badThreeSquared := 3 ^ 2
+	// fmt.Println("badThreeSquared:", badThreeSquared)
+	// goodThreeSquared := int(math.Pow(3.0, 2.0))
+	// fmt.Println("goodThreeSquare:", goodThreeSquared)
 
-	//modulus
+	// //modulus
 
-	remainder := 50 % 3
+	// remainder := 50 % 3
 
-	fmt.Println("remaider", remainder)
+	// fmt.Println("remaider", remainder)
 
+	// precedence
+	// multiplication
+	// a := 12.0 * 3.0 / 4.0
+	// b := (12.0 * 3.0) / 4.0
+	// c := 12 * (3.0 / 4.0)
+
+	// fmt.Println("a", a, "b", b, "c", c)
+
+	// // integer devision
+	// unclear := 12 * (3 / 4)
+	// fmt.Println("unclear", unclear)
+
+	// // parenthesis
+	// f := 12.0 / 3.0 / 4.0
+
+	// fmt.Println("f", f)
+
+	// f = 12.0 / (3.0 / 4.0)
+	// fmt.Println("f", f)
+
+	//does one number divide exactly into another?
+	// x := 12
+	// y := 3
+
+	// if x%y == 0 {
+	// 	fmt.Println(y, "divides exactly into", x)
+	// } else {
+	// 	fmt.Println(y, "does not divid exactly into", x)
+	// }
+
+	for m := 0; m <= 12; m++ {
+
+		fmt.Println("The month after", m, "is", m%12+1)
+
+	}
 }
